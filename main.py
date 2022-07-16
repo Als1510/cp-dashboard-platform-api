f3b0771eb3edd9be103f8fac9bf81169ed5f2c46
Als1510 <lekhrajsharmaas@gmail.com>
2022-07-16 23:23:08 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
-from flask_cors import CORS
 from collections.abc import Mapping
 from util import UserData, UsernameError, PlatformError, BrokenChangesError
 
 app = Flask(__name__)
-CORS(app)
 api = Api(app)
 
 class Details(Resource):
--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import json
-import json
 import re
 import requests
 from requests_html import HTMLSession
